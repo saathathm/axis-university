b5f7bbddb4c3cfde7245825fe4d0062fb469d95c
saathathm <saaadhath@gmail.com>
2026-05-02 21:19:39 +0530

    base path updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "node:url";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: process.env.VITE_BASE_PATH || "/axis-university",
+  base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
